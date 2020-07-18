4b50887750322044a104ac35cc0a493dd0c7d815
megan-pg <megan.pardygokcu@unh.edu>
2020-07-17 23:19:34 -0400

    fixed contact sheet

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+const submitBtn = "submit-button";
+
+
+submitBtn.addEventListener("click", sendEmail);
+
+ const sendEmail = function()
+
+
+//Data collected from text fields name, email, phone//
+
+//return to 
+
+//Identify text fields//
+
+
+
+
+
+//Data entered into storage//
+
+
+
+//Data parsed into generated email
+
+
+
+//Data sent to Megan
